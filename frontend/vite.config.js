e83b4aeee2a669ee67294c7b42ac6e7741c497ab
root <root@vmi1369754.contaboserver.net>
2025-05-12 15:44:46 +0200

    deployment update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: 'https://vps.eldc.dk/vp/',
   plugins: [solidPlugin()],
   server: {
     port: 3000,
-    allowedHosts: ["vps.edc.lol", "localhost", "127.0.0.1"],
+    allowedHosts: ["vps.eldc.dk", "localhost", "127.0.0.1"],
   },
   build: {
     target: 'esnext',
